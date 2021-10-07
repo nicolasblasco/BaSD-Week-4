b67020437987bd37d9a056d5fcdd143de9c04679
nicolasblasco <nicoblasco96@gmail.com>
2021-10-07 18:59:31 -0300

    Finished exercise 01

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,17 @@
+//a
+
+var num1 = 20;
+var num2 = 7;
+var sum = num1 + num2;
+
+//b
+
+var string1 = 'My';
+var string2 = 'app';
+var concatenation = string1 + string2
+
+//c
+
+var string1 = 'My';
+var string2 = 'app';
+var sumLength = string1.length + string2.length;
