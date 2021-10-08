898d2e9b4d95523e15ae2a334dfaf734b7553974
nicolasblasco <nicoblasco96@gmail.com>
2021-10-08 19:58:59 -0300

    Finished exercise 05: b

--- exercises/05_for.js
+++ exercises/05_for.js
@@ -7,10 +7,12 @@ for(i = 0; i < array.length; i++) {
 }
 
 //b
+//Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
 
 array = ['Requirements', 'design', 'implementation', 'Verification', 'Maintenance'];
 for(i = 0; i < array.length; i++) {
-
+    array[i] = array[i].substring(0,1).toLocaleUpperCase() + array[i].substring(1,array[i].length).toLocaleLowerCase();
+    alert(array[i]);
 }
 
 //c
