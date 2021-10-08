c64dd5ad1c5c3b9d582b36cb8f0d76c0d2783c4c
nicolasblasco <nicoblasco96@gmail.com>
2021-10-08 19:46:38 -0300

    Finished exercise 02: d, e & f

--- exercises/02_strings.js
+++ exercises/02_strings.js
@@ -5,15 +5,31 @@ console.log(stringUpperCase);
 
 //b 
 
-var string1 = "Lorem is a new app";
-var subString1 = string1.substring(0,5);
-console.log(subString1);
+var stringB = 'Lorem is a new app';
+var subStringB = stringB.substring(0,5);
+console.log(subStringB);
 
 //c
 
-var string2 = "Lorem is a new app";
-var subString2 = string2.substring(string2.length-3, string2.length);
-console.log(subString2);
+var stringC = 'Lorem is a new app';
+var subStringC = stringC.substring(stringC.length-3, stringC.length);
+console.log(subStringC);
 
 //d
 
+var stringD = 'lorem is a New App';
+var subStringD = stringD.substring(0,1).toLocaleUpperCase() + stringD.substring(1,stringD.length).toLocaleLowerCase();
+console.log(subStringD);
+
+//e 
+
+var stringE = 'Lorem is a new app';
+var firstBlankPoistion = stringE.indexOf(' ');
+console.log(firstBlankPoistion);
+
+//f
+
+var stringF = 'sofTwaRe DeveLoper'; 
+var index = stringF.indexOf(' ');
+var subStringF = stringF.substring(0, 1).toLocaleUpperCase() + stringF.substring(1,index).toLocaleLowerCase() + stringF.substring(index,stringF.length).toLocaleLowerCase();
+console.log(subStringF);
