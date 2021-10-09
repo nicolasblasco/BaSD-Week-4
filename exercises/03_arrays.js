94ea2b34c90858fc4172a3930582ad7cde828b45
nicolasblasco <nicoblasco96@gmail.com>
2021-10-08 22:04:49 -0300

    Modified console.logs for exercise 03

--- exercises/03_arrays.js
+++ exercises/03_arrays.js
@@ -1,40 +1,39 @@
 //a
 
 var array = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-console.log(array[4]);
-console.log(array[10]);
+console.log( '03-a: ',array[4], array[10]);
 
 //b
 
 array  = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-console.log(array.sort());
+console.log('03-b: ' + array.sort());
 
 //c
 
 array = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 array.unshift('Month');
 array.push('Year');
-console.log(array);
+console.log('03-c: ' + array);
 
 //d
 
 array = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 array.shift();
 array.pop();
-console.log(array);
+console.log('03-d: ' + array);
 
 //e
 
 array  = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-console.log(array.reverse());
+console.log('03-e: ' + array.reverse());
 
 //f
 
 array  = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-console.log(array.join(" - "));
+console.log('03-f: ' + array.join(" - "));
 
 //g 
 
 array  = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 arraySlice = array.slice(4,11);
-console.log(arraySlice);
+console.log('03-g: ' + arraySlice);
