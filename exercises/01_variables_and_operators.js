cdea2c03a162953cd135e64dbadc75af87fdcf95
nicolasblasco <nicoblasco96@gmail.com>
2021-10-08 21:54:10 -0300

    Modified console.logs exercise 01 again

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -10,11 +10,11 @@ console.log('01-a: The sum of the two numbers is ' + sum);
 var string1 = 'My ';
 var string2 = 'app';
 var concatenation = string1 + string2;
-console.log(' 01-b: The concatenation of the two strings is ' + concatenation);
+console.log('01-b: The concatenation of the two strings is ' + concatenation);
 
 //c
 
 var string1 = 'My';
 var string2 = 'app';
 var sumLength = string1.length + string2.length;
-console.log('01 -c: The sum of the length of the two strings is ' + sumLength);
+console.log('01-c: The sum of the length of the two strings is ' + sumLength);
