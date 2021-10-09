293eb4570dae397d97712972795e492e29c94b8e
nicolasblasco <nicoblasco96@gmail.com>
2021-10-08 21:52:01 -0300

    Added console.logs to exercise 01

--- exercises/01_variables_and_operators.js
+++ exercises/01_variables_and_operators.js
@@ -3,16 +3,18 @@
 var num1 = 20;
 var num2 = 7;
 var sum = num1 + num2;
+console.log('01-a: The sum of the two numbers is ' + sum);
 
 //b
 
 var string1 = 'My ';
 var string2 = 'app';
 var concatenation = string1 + string2;
+console.log('The concatenation of the two strings is ' + concatenation);
 
 //c
 
 var string1 = 'My';
 var string2 = 'app';
 var sumLength = string1.length + string2.length;
-git c
+console.log('The sum of the length of the two strings is ' + sumLength);
