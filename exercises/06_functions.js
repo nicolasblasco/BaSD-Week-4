3dbcb7cf5097c1ec5fea6ea72356493063bf6f0b
nicolasblasco <nicoblasco96@gmail.com>
2021-10-08 21:49:19 -0300

    Finisehd exercise 06: d & e

--- exercises/06_functions.js
+++ exercises/06_functions.js
@@ -28,21 +28,45 @@ x = 45;
 console.log(validateInteger(x));
 
 //d
-/*
+
 function sum3(a,b) {
     if (typeof(a) !== 'number' || typeof(b) !== 'number') {
-        alert('Error');
+        alert('Error: not a number');
+        return NaN;
     } else if (validateInteger(a) !== true) {
         alert('Error a');
-        return Math.round(a); 
+        a = Math.round(a); 
+        return a;
     } else if (validateInteger(b) !== true) {
         alert('Error b');
-        return Math.round(b); 
-
+        b = Math.round(b);
+        return b; 
     } else {
         return a + b;
     }
 }
-result3 = sum3(4.7,5);
-console.log(result3);
-*/
+
+//e 
+
+function validateNumber (number) {
+    if (typeof(num) !== 'number') {
+        alert('Error: not a number');
+        return false;
+    } 
+}
+
+function sum4(a,b) {
+    if (validateNumber(a) == false || validateNumber(b) == false) {
+        alert('Error: not a number')
+    } else if (validateInteger(a) !== true) {
+        alert('Error a');
+        a = Math.round(a); 
+        return a;
+    } else if (validateInteger(b) !== true) {
+        alert('Error b');
+        b = Math.round(b);
+        return b; 
+    } else {
+        return a + b;
+    }
+}
